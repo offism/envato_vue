2d581044d77fd72e882b22cf6f42b3d71f3bb72a
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-12-22 21:45:58 +0300

    2day 3st

--- src/components/Body/Services.vue
+++ src/components/Body/Services.vue
@@ -5,29 +5,29 @@
             <h2 class="p__title text-uppercase"><b>services</b></h2>
             <i class="p__text text-capitalize">ghettoba foundation</i>   
         </div>
-        <div class="raw">
+        <div class="raw d-flex py-5">
             <div class="services__type col-md-4">
                 <span class="fa-stack fa-4x">
-                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-circle fa-stack-2x"></i>
                     <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
                 </span>
-                <h4 class="my-3">Ghettoba Foundation</h4>
+                <h4 class="my-3"><b>Ghettoba Foundation</b></h4>
                 <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
             </div>   
             <div class="services__type col-md-4">
                 <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-circle fa-stack-2x"></i>
                             <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
                         </span>
-                        <h4 class="my-3">Responsive Design</h4>
+                        <h4 class="my-3"><b>Responsive Design</b></h4>
                         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
             </div>   
             <div class="services__type col-md-4">
                 <span class="fa-stack fa-4x">
-                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-circle fa-stack-2x"></i>
                     <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
                 </span>
-                    <h4 class="my-3">Web Security</h4>
+                    <h4 class="my-3"><b>Web Security</b></h4>
                     <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
             </div>   
         </div>
@@ -45,7 +45,7 @@ export default {
 .main__services{
     font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
     min-height: 100vh;
-    padding: 30px;
+    padding: 90px 30px;
     display: flex;
     text-align: center;
 }
@@ -56,5 +56,8 @@ export default {
 }
 .p__text{
     color:#6c757d;
+}
+.fa-circle{
+    color:#fed136;
 }
 </style>
