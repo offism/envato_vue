a4c6ed72282871ce397eab002e5d3990d099d366
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-12-23 22:35:29 +0300

    3day 1st

--- src/components/Body/About.vue
+++ src/components/Body/About.vue
@@ -13,7 +13,42 @@
                         <h4>2009-2011</h4>
                         <h4 class="subheading">Our Humble Beginnings</h4>
                     </div>
-                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed!</p></div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="./../../../public/img/about/2.jpg" alt="" /></div>
+                <div class="timeline-panel left-mixed">
+                    <div class="timeline-heading">
+                        <h4>March 2011</h4>
+                        <h4 class="subheading">An Agency is Born</h4>
+                    </div>
+                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed!</p></div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="./../../../public/img/about/3.jpg" alt="" /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4>December 2012</h4>
+                        <h4 class="subheading">Transition to Full Services</h4>
+                    </div>
+                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed!</p></div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="./../../../public/img/about/4.jpg" alt="" /></div>
+                <div class="timeline-panel left-mixed">
+                    <div class="timeline-heading">
+                        <h4>July 2011</h4>
+                        <h4 class="subheading">Phase Two Expansion</h4>
+                    </div>
+                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed!</p></div>
+                </div>
+            </li>
+            <li class="li__end">
+                <div class="timeline-image end">
+                  <h4 class="timeline_end__text">Be Part of Our Story!</h4>
                 </div>
             </li>
         </ul>
@@ -53,8 +88,9 @@ export default {
 .timeline:before {
   position: absolute;
   top: 0;
+  min-height: 80vh;
   bottom: 0;
-  left: 40px;
+  left: 50%;
   width: 2px;
   margin-left: -1.5px;
   content: "";
@@ -62,41 +98,31 @@ export default {
 }
 .timeline > li {
   position: relative;
-  min-height: 50px;
-  margin-bottom: 50px;
-}
-.timeline > li:after, .timeline > li:before {
-  display: table;
-  content: " ";
-}
-.timeline > li:after {
-  clear: both;
+  min-height: 200px;
+  margin-bottom: 200px;
 }
 .timeline > li .timeline-panel {
-  position: relative;
+  display: block;
+  position: absolute;
+  float: left;
+  width: 45%;
+  padding: 0 90px 0 100px;
+  text-align: right;
+}
+.timeline > li .timeline-panel.left-mixed {
+  display: block;
+  position: absolute;
   float: right;
-  width: 100%;
-  padding: 0 20px 0 100px;
-  text-align: left;
-}
-.timeline > li .timeline-panel:before {
-  right: auto;
-  left: -15px;
-  border-right-width: 15px;
-  border-left-width: 0;
-}
-.timeline > li .timeline-panel:after {
-  right: auto;
-  left: -14px;
-  border-right-width: 14px;
-  border-left-width: 0;
+  width: 45%;
+  left: 50%;
+text-align: left;
 }
 .timeline > li .timeline-image {
   position: absolute;
+  display: block;
   z-index: 100;
-  left: 0;
-  width: 80px;
-  height: 80px;
+  left: 41%;
+  width: 15%;
   margin-left: 0;
   text-align: center;
   color: white;
@@ -104,10 +130,22 @@ export default {
   border-radius: 100%;
   background-color: #fed136;
 }
-.timeline > li .timeline-image h4 {
-  font-size: 10px;
-  line-height: 14px;
-  margin-top: 12px;
+.li__end{
+  min-height: 160px !important;
 }
-
+.timeline > li .timeline-image.end{
+  height: 160px;
+  width: 160px;
+  margin-bottom: 0;
+  }
+.timeline > li .timeline-heading h4 {
+  font-size: 20px;
+  font-weight: 600;
+}
+.timeline_end__text{
+  font-weight: 600;
+  font-size: 22px;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  transform: translateY(90%);
+}
 </style>
