79f5ecd6cbad10ab9e63955775b2d0a3f392c1fb
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-11-11 16:07:23 +0300

    npm i else c1

--- public/js/AllMain.js
+++ public/js/AllMain.js
@@ -1,7 +1,6 @@
 let MyNavbar = document.querySelector("#mainNav");
 function abc() {
   document.addEventListener("scroll", (event) => {
-    console.log(event.path[1].pageYOffset, MyNavbar);
     if (event.path[1].pageYOffset > 0) {
       MyNavbar.style = "background: rgba(0,0,0,.8);";
     } else {
